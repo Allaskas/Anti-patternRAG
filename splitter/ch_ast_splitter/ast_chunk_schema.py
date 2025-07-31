5dc4f015f48dbbed4f55493cd305b0cf47676e98
MonCac <2500377984@qq.com>
2025-07-31 19:15:41 +0800

    1. 修复基类BaseChunk有默认值，子类LLMChunk和ASTChunk也必须有默认值问题

--- splitter/ch_ast_splitter/ast_chunk_schema.py
+++ splitter/ch_ast_splitter/ast_chunk_schema.py
@@ -4,4 +4,4 @@ from splitter.ch_ast_splitter.base_chunk_schema import BaseChunk
 
 @dataclass
 class ASTChunk(BaseChunk):
-    ast_subtree: str
+    ast_subtree: str = ""
--- splitter/ch_ast_splitter/llm_chunk_schema.py
+++ splitter/ch_ast_splitter/llm_chunk_schema.py
@@ -5,4 +5,4 @@ from splitter.ch_ast_splitter.base_chunk_schema import BaseChunk
 
 @dataclass
 class LLMChunk(BaseChunk):
-    llm_description: str
+    llm_description: str = ""
