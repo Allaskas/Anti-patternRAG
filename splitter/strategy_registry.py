49cd601222ce29cb44282fa255c5042a912414c2
MonCac <2500377984@qq.com>
2025-08-03 20:40:10 +0800

    实现spiltter和embeddings的批处理功能

--- splitter/strategy_registry.py
+++ splitter/strategy_registry.py
@@ -1,10 +1,12 @@
 def load_splitter_by_mode(mode):
     if mode == "java":
-        from java_only_splitter import build_chunk
+        from splitter.java_only_splitter import build_chunks
     elif mode == "json":
-        from json_only_splitter import build_chunk
+        from splitter.json_only_splitter import build_chunks
     elif mode == "json_java":
-        from json_java_splitter import build_chunk
+        from splitter.json_java_splitter import build_chunks
+    elif mode == "ast":
+        from splitter.ch_ast_splitter.ast_case_splitter import build_chunks
     else:
         raise ValueError(f"Unsupported mode: {mode}")
-    return build_chunk
+    return build_chunks
