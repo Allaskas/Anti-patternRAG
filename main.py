54b11622d8b314a575267dec5f99cec825c59b08
MonCac <2500377984@qq.com>
2025-12-05 19:23:08 +0800

    进行 mh 的 chunk 的编写

--- main.py
+++ main.py
@@ -26,12 +26,25 @@ if __name__ == "__main__":
     vectorstore_path = "/Users/moncheri/Downloads/main/重构/反模式修复数据集构建/RefactorRAG/Anti-PatternRAG/tmp/vectorstore"
     base_dir = "/Users/moncheri/Downloads/main/重构/反模式修复数据集构建/RefactorRAG/Anti-PatternRAG/tmp/merged_match_scores"
     # main()
-    # 进行 chunk
+    # 进行 CH chunk
     # chunk_all_cases(Path(DATA_DIR), "CH")
     # 对 chunk 的结果进行 embedding
     # embedding_all_chunks(Path(CHUNKS_DATA_DIR), "CH")
     # 直接对向量数据库的内容按照 CODE 和 TEXT 进行自评分
-    batch_process_vectorstore_query(vectorstore_path)
+    # batch_process_vectorstore_query(vectorstore_path)
+    # 对query 进行 chunk、embedding，并且进行最后的层级评分排序得到结果。
+    # 结果存储在 query/vectorstore/aggregated_result.json
+    # run_query_matching_pipeline("/data/sanglei/Anti-patternRAG/vectorstore", "/data/sanglei/Anti-patternRAG/query", 5)
+    # run_embedding_pipeline("/data/sanglei/Anti-patternRAG/data/CH/kafka/commit_1000/6/kafka_6_CH_chunk.json")
+    # run_embedding_pipeline("/data/sanglei/Anti-patternRAG/data/CH/kafka/commit_1000/6/kafka_6_CH_chunk.json")
+
+
+    # 进行 MH chunk
+    chunk_all_cases(Path(DATA_DIR), "MH")
+    # 对 chunk 的结果进行 embedding
+    # embedding_all_chunks(Path(CHUNKS_DATA_DIR), "CH")
+    # 直接对向量数据库的内容按照 CODE 和 TEXT 进行自评分
+    # batch_process_vectorstore_query(vectorstore_path)
     # 对query 进行 chunk、embedding，并且进行最后的层级评分排序得到结果。
     # 结果存储在 query/vectorstore/aggregated_result.json
     # run_query_matching_pipeline("/data/sanglei/Anti-patternRAG/vectorstore", "/data/sanglei/Anti-patternRAG/query", 5)
--- splitter/ch_ast_splitter/ast_case_splitter.py
+++ splitter/ch_ast_splitter/ast_case_splitter.py
@@ -12,6 +12,214 @@ from config.settings import ANTIPATTERN_TYPE
 
 
 def build_chunks(base_dir: Union[str, Path], antipattern_type, group_id):
+    result = ''
+    output_path = ''
+    match antipattern_type:
+        case "CH":
+            result, output_path = build_ch_chunks(base_dir, antipattern_type, group_id)
+        case "MH":
+            result, output_path = build_mh_chunks(base_dir, antipattern_type, group_id)
+        case "AWD":
+            result, output_path = build_awd_chunks(base_dir, antipattern_type, group_id)
+
+    return result, output_path
+
+
+def build_ch_chunks(base_dir: Union[str, Path], antipattern_type, group_id):
+    """
+    主函数：从一个 CH 案例文件夹中自动定位 JSON 和 Java 文件，抽取 AST 和分析块
+    :param base_dir: 指向某个具体 `{id}` 案例文件夹（包含 before/ 与 .json）
+    :param antipattern_type:
+    :param group_id:
+    :dict[str, Union[str, list, Any]]，每个父子子子块
+    """
+    base_dir = Path(base_dir)
+
+    print("enter build_chunks")
+
+    # 得到 chunk 的 metadata 内容，如果是构建 query 的chunk，即 group_id < 0，则其他内容都为-1
+    if group_id < 0:
+        antipattern_type = antipattern_type
+        project_name = -1
+        commit_number = -1
+        case_id = -1
+    else:
+        # 提取路径倒数四级
+        parts = base_dir.parts[-4:]  # 获取倒数4个路径名
+        _, project_name, commit_number, case_id = parts
+
+    # 找到 JSON 文件（一个案例文件夹应该只有一个 *antipattern.json）
+    json_files = list(base_dir.glob("*antipattern.json"))
+    if not json_files:
+        raise FileNotFoundError(f"No JSON name *antipattern.json file found in {base_dir}")
+
+    json_path = json_files[0]
+    case = load_case_info(json_path)
+
+    # 解析 2 个 Java 文件路径, 构建 Java 文件的完整路径（from before/ 目录）
+    java_base_dir = base_dir / "before"
+    super_path = os.path.join(java_base_dir, case.super_class_path)
+    sub_path = os.path.join(java_base_dir, case.sub_class_path)
+
+    with open(super_path, 'r', encoding='utf-8') as f:
+        super_code = f.read()
+
+    with open(sub_path, 'r', encoding='utf-8') as f:
+        sub_code = f.read()
+
+    # 提取位置信息（仅使用第一个调用）
+    snippet = case.code_snippets[0]
+    parent_method_loc = parse_line_range(snippet.parent_method.location)
+    child_method_loc = parse_line_range(snippet.child_method.location)
+    invocation_loc = parse_line_range(snippet.invocation.location)
+
+    # 提取代码片段
+    parent_method_code = snippet.parent_method.code
+    child_method_code = snippet.child_method.code
+    invocation_code = snippet.invocation.code
+
+    # ---- SuperClass 的子块 ----
+    print("start SuperClass Chunk")
+    super_chunks: List[BaseChunk] = []
+    super_chunks = extract_superclass_chunks(super_code, super_path, parent_method_loc, invocation_loc)
+    super_chunks.extend(llm_analyze_superclass(super_path, parent_method_code, invocation_code))
+
+    # ---- SubClass 的子块 ----
+    print("start SubClass Chunk")
+    sub_chunks: List[BaseChunk] = []
+    sub_chunks = extract_subclass_chunks(sub_code, sub_path, child_method_loc)
+    sub_chunks.extend(llm_analyze_subclass(sub_path, child_method_code))
+
+    # ---- 整合所有 chunk ----
+    all_chunks = super_chunks + sub_chunks
+    print(all_chunks)
+
+    json_chunks = [chunk.to_dict() for chunk in all_chunks]
+
+    result = {
+        "antipattern_type": antipattern_type,
+        "project_name": project_name,
+        "commit_number": commit_number,
+        "id": case_id,
+        "group_id": group_id,
+        "chunks": json_chunks
+    }
+
+    if group_id < 0:
+        chunk_filename = "query_chunk.json"
+        output_dir = Path("query")
+    else:
+        # 构造输出路径：和 JSON 文件在同一目录，命名为 `{project}_{case_id}_{antipattern}_chunk.json`
+        chunk_filename = f"{project_name}_{commit_number}_{case_id}_{antipattern_type}_chunk.json"
+        output_dir = Path(f"tmp/chunks/{antipattern_type}")
+    output_dir.mkdir(parents=True, exist_ok=True)  # 自动创建目录
+    output_path = output_dir / chunk_filename
+
+    with open(output_path, "w", encoding="utf-8") as f:
+        json.dump(result, f, ensure_ascii=False, indent=2)
+
+    print(f"分块结果已保存至: {output_path}")
+    return result, output_path
+
+
+def build_mh_chunks(base_dir: Union[str, Path], antipattern_type, group_id):
+    """
+    主函数：从一个 MH 案例文件夹中自动定位 JSON 和 Java 文件，抽取 AST 和分析块
+    :param base_dir: 指向某个具体 `{id}` 案例文件夹（包含 before/ 与 .json）
+    :param antipattern_type:
+    :param group_id:
+    :dict[str, Union[str, list, Any]]，每个父子子子块
+    """
+    base_dir = Path(base_dir)
+
+    print("enter build_chunks")
+
+    # 得到 chunk 的 metadata 内容，如果是构建 query 的chunk，即 group_id < 0，则其他内容都为-1
+    if group_id < 0:
+        antipattern_type = antipattern_type
+        project_name = -1
+        commit_number = -1
+        case_id = -1
+    else:
+        # 提取路径倒数四级
+        parts = base_dir.parts[-4:]  # 获取倒数4个路径名
+        _, project_name, commit_number, case_id = parts
+
+    # 找到 JSON 文件（一个案例文件夹应该只有一个 *antipattern.json）
+    json_files = list(base_dir.glob("*antipattern.json"))
+    if not json_files:
+        raise FileNotFoundError(f"No JSON name *antipattern.json file found in {base_dir}")
+
+    json_path = json_files[0]
+    case = load_case_info(json_path)
+
+    # 解析 2 个 Java 文件路径, 构建 Java 文件的完整路径（from before/ 目录）
+    java_base_dir = base_dir / "before"
+    super_path = os.path.join(java_base_dir, case.super_class_path)
+    sub_path = os.path.join(java_base_dir, case.sub_class_path)
+
+    with open(super_path, 'r', encoding='utf-8') as f:
+        super_code = f.read()
+
+    with open(sub_path, 'r', encoding='utf-8') as f:
+        sub_code = f.read()
+
+    # 提取位置信息（仅使用第一个调用）
+    snippet = case.code_snippets[0]
+    parent_method_loc = parse_line_range(snippet.parent_method.location)
+    child_method_loc = parse_line_range(snippet.child_method.location)
+    invocation_loc = parse_line_range(snippet.invocation.location)
+
+    # 提取代码片段
+    parent_method_code = snippet.parent_method.code
+    child_method_code = snippet.child_method.code
+    invocation_code = snippet.invocation.code
+
+    # ---- SuperClass 的子块 ----
+    print("start SuperClass Chunk")
+    super_chunks: List[BaseChunk] = []
+    super_chunks = extract_superclass_chunks(super_code, super_path, parent_method_loc, invocation_loc)
+    super_chunks.extend(llm_analyze_superclass(super_path, parent_method_code, invocation_code))
+
+    # ---- SubClass 的子块 ----
+    print("start SubClass Chunk")
+    sub_chunks: List[BaseChunk] = []
+    sub_chunks = extract_subclass_chunks(sub_code, sub_path, child_method_loc)
+    sub_chunks.extend(llm_analyze_subclass(sub_path, child_method_code))
+
+    # ---- 整合所有 chunk ----
+    all_chunks = super_chunks + sub_chunks
+    print(all_chunks)
+
+    json_chunks = [chunk.to_dict() for chunk in all_chunks]
+
+    result = {
+        "antipattern_type": antipattern_type,
+        "project_name": project_name,
+        "commit_number": commit_number,
+        "id": case_id,
+        "group_id": group_id,
+        "chunks": json_chunks
+    }
+
+    if group_id < 0:
+        chunk_filename = "query_chunk.json"
+        output_dir = Path("query")
+    else:
+        # 构造输出路径：和 JSON 文件在同一目录，命名为 `{project}_{case_id}_{antipattern}_chunk.json`
+        chunk_filename = f"{project_name}_{commit_number}_{case_id}_{antipattern_type}_chunk.json"
+        output_dir = Path(f"tmp/chunks/{antipattern_type}")
+    output_dir.mkdir(parents=True, exist_ok=True)  # 自动创建目录
+    output_path = output_dir / chunk_filename
+
+    with open(output_path, "w", encoding="utf-8") as f:
+        json.dump(result, f, ensure_ascii=False, indent=2)
+
+    print(f"分块结果已保存至: {output_path}")
+    return result, output_path
+
+
+def build_awd_chunks(base_dir: Union[str, Path], antipattern_type, group_id):
     """
     主函数：从一个 CH 案例文件夹中自动定位 JSON 和 Java 文件，抽取 AST 和分析块
     :param base_dir: 指向某个具体 `{id}` 案例文件夹（包含 before/ 与 .json）
